9ce3b124aa51dff06b24193b72d58f2bbf66d077
Nazar Chyburovskyi <nchiburovskiy@gmail.com>
2024-08-30 09:38:08 +0300

    save

---
+++ 1pract.py
@@ -0,0 +1,24 @@
+print("Привіт, світе!")
+
+def calc():
+    while True:
+        num1 = int(input("enter num1: "))
+        num2 = int(input("enter num2: "))
+        operator = input("choose +, -, *, / or 0 to exit: ")
+        match operator:
+            case "+":
+                print(num1 + num2)
+            case "-":
+                print(num1 - num2)
+            case "*":
+                print(num1 * num2)
+            case "/":
+                if num2 != 0:(
+                    print(float(num1 / num2)))
+                else: 
+                    print("division by 0 unavalible")
+            case "0":
+                break
+            case _: 
+                print("error")
+calc()
