6eb8b7919f3a595fd63104011a154846e55c0fe3
Nazar Chyburovskyi <nchiburovskiy@gmail.com>
2024-10-11 10:12:41 +0300

    save

--- 5pract/map_visualization.py
+++ 5pract/map_visualization.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
 import plotly.express as px
+from matplotlib.ticker import FuncFormatter
 
 def static_map(merged_data):
-    fig, ax = plt.subplots(1, 1, figsize=(15, 10))
+    fig, ax = plt.subplots(1, 1, figsize=(16, 10))
     merged_data.plot(column='Total Cases', cmap='OrRd', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
+    cbar = ax.get_figure().get_axes()[1]
+    cbar.yaxis.set_major_formatter(FuncFormatter(lambda x, _: f'{int(x / 1_000_000)}M'))
     ax.set_title('Кількість випадків COVID-19 по країнах', fontsize=15)
     plt.show()
 
@@ -14,7 +17,7 @@ def interactive_map(merged_data):
         locationmode="country names",
         color="Total Cases",
         hover_name="Country",
-        hover_data=["Total Deaths"],
+        hover_data={"Total Cases": ":,.0f", "Total Deaths": ":,.0f"},
         title="Кількість випадків COVID-19 по країнах",
         color_continuous_scale="OrRd",
         labels={'Total Cases': 'Випадки', 'Total Deaths': 'Смерті'}
