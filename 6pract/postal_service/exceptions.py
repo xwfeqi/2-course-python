cc36d3e63096f36944f61c3e714cec710e7d1002
Nazar Chyburovskyi <nchiburovskiy@gmail.com>
2024-10-16 15:37:18 +0300

    save

--- 6pract/postal_service/exceptions.py
+++ 6pract/postal_service/exceptions.py
@@ -1,7 +1,7 @@
 class InvalidParcelException(Exception):
-    """Виняток для некоректних посилок."""
+    print("Виняток для некоректних посилок.")
     pass
 
 class ParcelNotFoundException(Exception):
-    """Виняток для посилки, яку не знайдено."""
+    print("Виняток для посилки, яку не знайдено.")
     pass
